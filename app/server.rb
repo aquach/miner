14177755d554d796aa333aa5bd373e65657da583
Alex Quach <alexhquach@gmail.com>
2014-12-28 22:29:57 -0800

    Cleaning up toolchain.

--- app/server.rb
+++ app/server.rb
@@ -8,72 +8,8 @@ require 'sinatra'
 set :port, 8000
 set :views, :less => 'css', :default => 'views'
 
-THIRD_PARTY_JS_FILES = [
-  'underscore-min',
-  '**/*'
-]
-
-ENGINE_JS_FILES = [
-  'setup',
-  'engine/util',
-  'engine/workers',
-  'engine/money',
-  'engine/**/*'
-]
-
-UI_JS_FILES = [
-  'ui/**/*',
-  'main'
-]
-
-SPEC_FILES = [
-  '**/*'
-]
-
-helpers do
-  #def find_template(views, name, engine, &block)
-  #  _, folder = views.detect { |k,v| engine == Tilt[k] }
-  #  folder ||= views[:default]
-  #  super(folder, name, engine, &block)
-  #end
-
-  def find_js(globs, dir)
-    globs.map { |glob| Dir[dir + glob + '.js'] }.flatten.uniq
-  end
-
-  def third_party_js_files
-    find_js(THIRD_PARTY_JS_FILES, 'public/third-party-js').map { |filename|
-      filename.split('/').drop(1).join('/')
-    }
-  end
-
-  def engine_files
-    find_js(ENGINE_JS_FILES, 'js/game/')
-  end
-
-  def ui_files
-    find_js(UI_JS_FILES, 'js/game/')
-  end
-
-  def app_files
-    engine_files + ui_files
-  end
-
-  def spec_files
-    find_js(SPEC_FILES, 'js/specs/')
-  end
-end
-
-get '/main' do
+get '/' do
   haml :main
-end
-
-get '/js/*' do |path|
-  coffee path.gsub(/\..*$/, '').to_sym
-end
-
-get '/specs' do
-  haml :specs
 end
 
 get '/css/*' do |path|
